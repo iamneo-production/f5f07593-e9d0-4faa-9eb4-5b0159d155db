9fcdc8dcba96ae4d8919236289b04a0897cf3511
Tamil033 <tamilsubu033@gmail.com>
2023-05-01 20:36:47 +0000

    Initial Commit

--- dotnetapp/Models/ResponseModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace dotnetapp
-{
-    public class ResponseModel
-    {
-        public string Message { get; internal set; }
-        public bool Status { get; internal set; }
-        public object ErrorMessage { get; internal set; }
-    }
-}
